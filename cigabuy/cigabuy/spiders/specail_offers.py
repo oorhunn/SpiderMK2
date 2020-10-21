c4dcc2ab361f313e76f370408b3e472ed1c70835
oorhunn <49835142+oorhunn@users.noreply.github.com>
2020-10-21 16:56:42 +0300

    settings.py be careful man

--- cigabuy/cigabuy/spiders/specail_offers.py
+++ cigabuy/cigabuy/spiders/specail_offers.py
@@ -3,8 +3,14 @@ import scrapy
 
 class SpecailOffersSpider(scrapy.Spider):
     name = 'specail_offers'
-    allowed_domains = ['www.cigabuy.com/weekly-deals-c-56_187.html']
-    start_urls = ['https://www.cigabuy.com/weekly-deals-c-56_187.html/']
+    allowed_domains = ['www.cigabuy.com']
+    start_urls = ['https://www.cigabuy.com/weekly-deals-c-56_187.html']
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//div[@class='p_box_wrapper']"):
+            yield {
+                'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discounted_price': product.xpath(".//div[@class='p_box_price cf']/span[1]/text()").get(),
+                'original_price': product.xpath(".//div[@class='p_box_price cf']/span[2]/text()").get()
+            }
