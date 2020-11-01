11f7f4257964f0749ca42f8dbb02fe7bb7ab3303
oorhunn <49835142+oorhunn@users.noreply.github.com>
2020-11-01 15:05:13 +0300

    starting new project using splash and scrapy together tutorial

---
+++ livecoin/livecoin/spiders/coin.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class CoinSpider(scrapy.Spider):
+    name = 'coin'
+    allowed_domains = ['www.livecoin.net/en']
+    start_urls = ['http://www.livecoin.net/en/']
+
+    def parse(self, response):
+        pass
