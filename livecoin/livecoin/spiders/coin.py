90b5dbe6f55b1e88c445c831feda8ace055a7626
oorhunn <49835142+oorhunn@users.noreply.github.com>
2020-11-01 15:18:25 +0300

    some basic adjustments for splash scrapy together

--- livecoin/livecoin/spiders/coin.py
+++ livecoin/livecoin/spiders/coin.py
@@ -1,10 +1,28 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class CoinSpider(scrapy.Spider):
     name = 'coin'
     allowed_domains = ['www.livecoin.net/en']
-    start_urls = ['http://www.livecoin.net/en/']
+
+    script = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(6))
+            rur_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
+            rur_tab[5]:mouse_click()
+            assert(splash:wait(5))
+            splash:set_viewport_full()
+            return splash:png()
+        end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url="https://www.livecoin.net/en", callback=self.parse, endpoint="execute", args={
+            'lua_source': self.script
+        })
 
     def parse(self, response):
-        pass
+        print(response.body)
