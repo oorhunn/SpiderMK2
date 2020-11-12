4047063b50a8c4d41d7a4ab12a3bd167dceaad96
oorhunn <49835142+oorhunn@users.noreply.github.com>
2020-11-12 13:26:36 +0300

    ebook api stuff

---
+++ demo_api/demo_api/spiders/ebooks.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy.exceptions import CloseSpider
+import json
+
+
+class EbooksSpider(scrapy.Spider):
+    name = 'ebooks'
+    allowed_domains = ['openlibrary.org']
+    start_urls = ['https://openlibrary.org/subjects/picture_books.json?limit=12']
+
+    INCREMENTED_BY = 12
+    offset = 0
+    def parse(self, response):
+        if response.status ==500:
+            CloseSpider('Reached last page ')
+
+        resp = json.loads(response.body)
+        ebooks = resp.get('works')
+        for ebook in ebooks:
+            yield {
+                'title': ebook.get('title'),
+                'subject': ebook.get('subject')
+            }
+
+        self.offset += self.INCREMENTED_BY
+        yield scrapy.Request(
+            url=f'https://openlibrary.org/subjects/picture_books.json?limit={self.offset}',
+            callback=self.parse
+            )
